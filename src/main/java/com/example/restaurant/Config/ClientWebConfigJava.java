33517fcad8d12095099763f7c718f7bba8050ad8
Aleksey <allenb495@gmail.com>
2023-11-24 20:32:38 +0300

    security upd

--- src/main/java/com/example/restaurant/Config/ClientWebConfigJava.java
+++ src/main/java/com/example/restaurant/Config/ClientWebConfigJava.java
@@ -1,4 +1,4 @@
-package com.example.restaurant.WebMVC;
+package com.example.restaurant.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
---
+++ src/main/java/com/example/restaurant/Config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.restaurant.Config;
+
+import com.example.restaurant.controllers.UserController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@EnableWebMvc
+public class WebSecurityConfig {
+    @Autowired
+    private UserController userController;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/",
+                                "/api/vi/auth/login**",
+                                "/api/vi/auth/signup**",
+                                "/api/vi/auth/logout**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
