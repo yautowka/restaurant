9f7c2f0f50cc8703b6c4dac9d8a9b5bce23584d8
Aleksey <allenb495@gmail.com>
2024-01-31 14:55:56 +0300

    chipi chipi

--- src/main/java/com/example/restaurant/config/PasswordConfig.java
+++ src/main/java/com/example/restaurant/config/PasswordConfig.java
@@ -5,7 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+/*
+Chipi chipi chapa chapa
+Dubi dubi daba daba
+Mágico mi dubi dubi
+Bum, bum, bum, bum
+Chipi chipi chapa chapa
+Dubi dubi daba daba
+Mágico mi dubi dubi
+Bum
+*/
 
 @Configuration
 @RequiredArgsConstructor
